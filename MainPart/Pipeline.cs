8f95456fa149d5cb7eb85d684f47c9991b3c821e
Lenovo <d.shparko@mail.ru>
2021-12-10 20:37:00 +0300

    add main part

---
+++ MainPart/Pipeline.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using TestGeneratorLib;
+
+namespace MainPart
+{
+    public class Pipeline
+    {
+        public Task Generate(IEnumerable<string> files, string pathToGenerated)
+        {
+            var execOptions = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = files.Count() };
+            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+            var downloadStringBlock = new TransformBlock<string, string>
+            (
+                async path =>
+                {
+                    using (var reader = new StreamReader(path))
+                    {
+                        return await reader.ReadToEndAsync();
+                    }
+                },
+                execOptions
+            );
+            var generateTestsBlock = new TransformManyBlock<string, KeyValuePair<string, string>>
+            (
+                async sourceCode =>
+                {
+                    var fileInfo = await Task.Run(() => CodeAnalyzer.GetFileInfo(sourceCode));
+                    Console.WriteLine("zzz");
+                    return await Task.Run(() => TestsGenerator.GenerateTests(fileInfo));
+                },
+                execOptions
+            );
+            var writeFileBlock = new ActionBlock<KeyValuePair<string, string>>
+            (
+                async fileNameCodePair =>
+                {
+                    Console.WriteLine("ohoho");
+                    using (var writer = new StreamWriter(pathToGenerated + '\\' + fileNameCodePair.Key + ".cs"))
+                    {
+                        Console.WriteLine("hhh"+fileNameCodePair.Key);
+                        await writer.WriteAsync(fileNameCodePair.Value);
+                    }
+                },
+                execOptions
+            );
+            downloadStringBlock.LinkTo(generateTestsBlock, linkOptions);
+            generateTestsBlock.LinkTo(writeFileBlock, linkOptions);
+
+            foreach (var file in files)
+            {
+                downloadStringBlock.Post(file);
+                Console.WriteLine(file);
+            }
+
+            downloadStringBlock.Complete();
+            return writeFileBlock.Completion;
+        }
+    }
+}
---
+++ MainPart/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MainPart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pathToFolder = "C:\\Users\\Lenovo\\OneDrive - bsuir.by\\Рабочий стол\\TestGenerator\\MainPart\\Files";
+            var pathToGenerated = @"C:\Users\Lenovo\OneDrive - bsuir.by\Рабочий стол\TestGenerator\GeneratedTests\GeneratedFiles";
+            
+            if (!Directory.Exists(pathToFolder))
+            {
+                Console.WriteLine($"Couldn't find directory {pathToFolder}");
+                return;
+            }
+            if (!Directory.Exists(pathToGenerated))
+            {
+                Directory.CreateDirectory(pathToGenerated);
+            }
+
+            var allFiles = Directory.GetFiles(pathToFolder);
+
+            var files = from file in allFiles
+                    where file.Substring(file.Length - 3) == ".cs"
+                    select file;
+
+            Task task =  new Pipeline().Generate(files, pathToGenerated);
+            task.Wait();
+            //Thread.Sleep(2000);
+            Console.WriteLine("end.");
+        }
+    }
+}
